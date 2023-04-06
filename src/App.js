2905fb4faa40b9747247216a58b077cedd7bf2f8
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-04-06 17:24:07 +0200

    tailwind css styling cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
+import './index.css';
 import Skill from './components/Skill';
+import { useState } from 'react';
 
 function App() {
   const showSkills = true;
@@ -7,28 +8,17 @@ function App() {
     <div className="App">
       {showSkills ? (
         <>
-          <Skill
-            name="HTML"
-            utility="HTML is the standard markup language for documents designed to be displayed in a web browser."
-            level="Mastered"
-          />
-          <Skill
-            name="CSS"
-            utility="CSS is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML."
-            level="Mastered"
-          />
-          <Skill
-            name="JS"
-            utility="JS is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS"
-          />
-          <Skill
-            name="NODE JS"
-            utility="Node.js is a back-end JavaScript runtime environment, runs on the V8 JavaScript Engine, and executes JavaScript code outside a web browser."
-          />
-          <Skill
-            name="REACT"
-            utility="React is a free and open-source front-end JavaScript library for building user interfaces based on components."
-          />
+          <div className="flex flex-wrap">
+            <Skill
+              name="HTML"
+              level="Mastered"
+              img="https://images.unsplash.com/photo-1632882765546-1ee75f53becb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHRtbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            />
+            <Skill name="CSS" level="Mastered" />
+            <Skill name="JS" level="Learning in progress" />
+            <Skill name="NODE JS" level="Learning in progress" />
+            <Skill name="REACT" level="Learning in progress" />
+          </div>
         </>
       ) : (
         <p>You cannot see the skills</p>
